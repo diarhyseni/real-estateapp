ccd3cb0945a0c5010500aa2c0a3486d754b0d467
Diar Hyseni <diarhyseni4@gmail.com>
2025-06-14 00:50:13 +0200

    fixed

--- app/api/categories/[id]/route.ts
+++ app/api/categories/[id]/route.ts
@@ -1,19 +1,25 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/lib/db'
 
-export async function GET(request: Request, { params }: { params: { id: string } }) {
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
   const category = await prisma.category.findUnique({
     where: { id: params.id },
     include: {
       _count: {
-        select: { Property: true }
+        select: { properties: true }
       }
     }
   })
   return NextResponse.json(category)
 }
 
-export async function PUT(request: Request, { params }: { params: { id: string } }) {
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
   const data = await request.json()
   const category = await prisma.category.update({
     where: { id: params.id },
@@ -22,7 +28,10 @@ export async function PUT(request: Request, { params }: { params: { id: string }
   return NextResponse.json(category)
 }
 
-export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
   await prisma.category.delete({ where: { id: params.id } })
   return NextResponse.json({ success: true })
 } 
