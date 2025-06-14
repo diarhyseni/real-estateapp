33d8b7d936cdbc5d766969592b5eae483f2ec42c
Diar Hyseni <diarhyseni4@gmail.com>
2025-06-14 11:11:15 +0200

    fixed37

--- types/bcryptjs.d.ts
+++ types/bcryptjs.d.ts
@@ -1,4 +1,9 @@
 declare module 'bcryptjs' {
-  export function hash(data: string, saltOrRounds: string | number): Promise<string>;
-  export function compare(data: string, encrypted: string): Promise<boolean>;
+  export function hash(data: string | Buffer, saltOrRounds: string | number): Promise<string>;
+  export function compare(data: string | Buffer, encrypted: string): Promise<boolean>;
+  export function genSalt(rounds?: number): Promise<string>;
+  export function getRounds(hash: string): number;
+  export function hashSync(data: string | Buffer, saltOrRounds: string | number): string;
+  export function compareSync(data: string | Buffer, encrypted: string): boolean;
+  export function genSaltSync(rounds?: number): string;
 } 
