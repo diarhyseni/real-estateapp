ea88d5efae04fb950354f97456f68d529de7f621
Diar Hyseni <diarhyseni4@gmail.com>
2025-06-15 14:46:42 +0200

    calendar update

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -54,8 +54,12 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        Chevron: ({ className, orientation, ...props }) => {
+          if (orientation === 'left') {
+            return <ChevronLeft className={cn("h-4 w-4", className)} {...props} />;
+          }
+          return <ChevronRight className={cn("h-4 w-4", className)} {...props} />;
+        },
       }}
       {...props}
     />
